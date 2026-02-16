5a5010c95de45d4df4933eba19d03c3b7a5cce1e
Saurabh Chandra <saurabhchandra1244@gmail.com>
2026-02-17 00:50:16 +0530

    port ts solved

--- src/app.ts
+++ src/app.ts
@@ -127,8 +127,9 @@ app.get("/tickets", async (_req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
