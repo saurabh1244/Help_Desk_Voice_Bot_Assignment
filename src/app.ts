fb8142013dab1d02f0c81d4e98c10a6350a4a451
Saurabh Chandra <saurabhchandra1244@gmail.com>
2026-02-17 00:41:23 +0530

    Prepare for Render deploy

--- src/app.ts
+++ src/app.ts
@@ -127,7 +127,8 @@ app.get("/tickets", async (_req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
